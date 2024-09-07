4a763971209c29cd0fd5aefa2025760a2546152e
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-07 17:08:10 +0800

    Fix app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Extracurriculars from './components/Extracurriculars';
+import Writing from './components/Writing';
+
 
 function App() {
   return (
