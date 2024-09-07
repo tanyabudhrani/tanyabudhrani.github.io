a5a50ff2f152e1950daa03e5b04437889624402f
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-07 15:42:14 +0800

    j

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Skills from './components/Skills';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Extracurriculars from './components/Extracurriculars';
-import Writing from './components/Writing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="bg-gray-900 text-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/extracurriculars" element={<Extracurriculars />} />
-          <Route path="/writing" element={<Writing />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/extracurriculars" element={<Extracurriculars />} />
+        <Route path="/writing" element={<Writing />} />
+      </Routes>
     </Router>
   );
 }
