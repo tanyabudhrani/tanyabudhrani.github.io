8e2ddf8382e9a9d515e99da40384dc6b9640d173
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-08 19:17:19 +0800

    Check

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,57 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: '#0a192f',  // Dark blue background
+//         secondary: '#64ffda',  // Light green/teal accents for buttons and highlights
+//         tertiary: '#112240',  // Slightly lighter dark blue for section backgrounds
+//         light: '#ccd6f6',  // Light gray/blue for text
+//         dark: '#8892b0',  // Muted gray for secondary text
+//         accent: '#ff007f',  // Even lighter blue for subtle accents
+//       },
+//       fontFamily: {
+//         sans: ['Calibre', 'Inter', 'sans-serif'],  // You can replace this with the font used on the site
+//       },
+//     },
+//     backgroundImage: {
+//       'gradient-radial': 'radial-gradient(circle, rgba(10, 25, 47, 1) 0%, rgba(34, 48, 63, 1) 100%)',
+//     },
+//   },
+//   plugins: [],
+//   variants: {
+//     extend: {
+//       // smooth scrolling
+//       scrollBehavior: ['motion-safe', 'motion-reduce'],
+//     },
+//   }
+// }
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
     extend: {
       colors: {
-        primary: '#0a192f',  // Dark blue background
-        secondary: '#64ffda',  // Light green/teal accents for buttons and highlights
-        tertiary: '#112240',  // Slightly lighter dark blue for section backgrounds
-        light: '#ccd6f6',  // Light gray/blue for text
-        dark: '#8892b0',  // Muted gray for secondary text
-        accent: '#ff007f',  // Even lighter blue for subtle accents
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
       fontFamily: {
-        sans: ['Calibre', 'Inter', 'sans-serif'],  // You can replace this with the font used on the site
+        poppins: ["Poppins", "sans-serif"],
       },
     },
-    backgroundImage: {
-      'gradient-radial': 'radial-gradient(circle, rgba(10, 25, 47, 1) 0%, rgba(34, 48, 63, 1) 100%)',
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
@@ -28,4 +61,4 @@ module.exports = {
       scrollBehavior: ['motion-safe', 'motion-reduce'],
     },
   }
-}
+};
