c94d5ef9f8c4ca2f56a5e11aaaf13815df2b18e6
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-08 20:37:13 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Home() {
   return (
     <section id="home" className="min-h-screen flex flex-col justify-center items-center bg-gray-900">
-      <h1 className="text-5xl font-bold mb-4">Tanya Budhrani</h1>
-      <p className="text-2xl mb-8">ML Researcher | Computer Scientist | Linguist</p>
+      <h1 className="text-5xl font-bold mb-4 text-white">Tanya Budhrani</h1>
+      <p className="text-2xl mb-8 text-white" >ML Researcher | Computer Scientist | Linguist</p>
       <div className="flex space-x-4">
         <a href="https://github.com/tanyabudhrani" target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           GitHub
