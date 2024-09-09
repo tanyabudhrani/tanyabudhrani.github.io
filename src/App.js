87a59da52705c81f44df4cc844ede161295a53af
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-09 12:19:47 +0800

    Fixed Ecs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,4 @@
-// import Home from './components/Home';
-// import About from './components/About';
-// import Skills from './components/Skills';
-// import Experience from './components/Experience';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-// import Extracurriculars from './components/Extracurriculars';
-// import Writing from './components/Writing';
-// import Navbar from './components/Navbar';
 
-// function App() {
-//   return (
-//     <div className ="bg-gradient-to-b from-gray-900 via-teal-900 to-black">"
-//       <Navbar />
-//       <Home />
-//       <About />
-//       <Skills />
-//       <Experience />
-//       <Projects />
-//       <Extracurriculars />
-//       <Writing />
-//       <Contact />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React from "react";
 import { motion } from "framer-motion";
