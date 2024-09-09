d718bb412e110a63320f1982f41ead3165a6d88c
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-09 22:57:34 +0800

    Fixed about

--- src/components/About.js
+++ src/components/About.js
@@ -12,12 +12,15 @@ const About = () => {
             alt="Tanya Budhrani"
             className="w-48 h-48 rounded-full mb-6 md:mb-0 md:mr-12"
           />
-          <div className="text-center">
+          <div className="ml-auto mr-auto md:ml-20">
             <p className="text-xl md:text-2xl mb-4 hover:bubble">
-              Hi!, I'm <strong>Tans</strong>
+              Hello there, I'm <strong>Tans</strong>
             </p>
             <p className="text-md md:text-lg mb-4 hover:bubble">
-            As a passionate computer science student with a keen interest in Natural Language Processing and Large Language Models, I bring over six years of hands-on programming experience, aiming to leverage my skills in delivering innovative solutions to problems unexplored. 
+              A final-year Computer Science and Linguistics student at the Hong Kong Polytechnic University with a keen interest in Natural Language Processing and Machine Learning.
+            </p>
+            <p className="text-md md:text-lg hover:bubble">
+              When I'm not coding or studying languages, you can find me involved in various campus groups, from tech clubs to language exchange programs, pushing the boundaries of what we can achieve with technology and communication.
             </p>
             <a
               href="https://drive.google.com/file/d/15LZ11bRZXB91_zkGCiLQoVu17GRVcqWE/view?usp=drive_link"
@@ -35,4 +38,3 @@ const About = () => {
 };
 
 export default About;
-
