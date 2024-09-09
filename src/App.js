ae97abd08e817317ab4c6fe09e4a1e12d15d543b
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-09 17:09:29 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,22 +24,21 @@ const App = () => {
 
   if (isLoading) {
     return (
-      <div className="bg-dark w-full overflow-hidden">
-        {/* You can replace Loading with an actual loading component */}
+      <div className="bg-primary w-full overflow-hidden">
         <div className="text-white flex items-center justify-center h-screen">Tanya Budhrani</div>
       </div>
     );
   } else {
     return (
       // A div to wrap the entire application with a gradient background
-      <div className="bg-gradient-to-b w-full overflow-hidden">
+      <div className="bg-primary w-full overflow-hidden">
         <motion.section
           initial={{ x: -100, opacity: 0.25 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.5 }}
         >
           {/* Navbar and Home Section */}
-          <div className="bg-dark flex justify-center items-center">
+          <div className="bg-primary flex justify-center items-center">
             <div className="w-full max-w-screen-xl">
               <Hero />
               <Navbar />
@@ -47,7 +46,7 @@ const App = () => {
           </div>
 
           {/* Experience and Education Section */}
-          <div className="bg-dark flex justify-center px-6">
+          <div className="bg-primary flex justify-center px-6">
             <div className="w-full max-w-screen-xl">
               <Experience />
               {/*<Education /> */}
@@ -55,7 +54,7 @@ const App = () => {
           </div>
 
           {/* Projects, Writing, and Extracurriculars Section */}
-          <div className="bg-dark flex justify-center px-6">
+          <div className="bg-primary flex justify-center px-6">
             <div className="w-full max-w-screen-xl">
               <Projects />
               <Writing />
@@ -64,7 +63,7 @@ const App = () => {
           </div>
 
           {/* Contact Section */}
-          <div className="bg-dark w-full max-w-screen-xl">
+          <div className="bg-primary w-full max-w-screen-xl">
             <Contact />
           </div>
         </motion.section>
