fc392dba4381a4133b378c04e49dd484ee71c372
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-10 16:22:33 +0800

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Contact() {
   return (
-    <section id="contact" className="py-20 bg-secondary text-white">
+    <section id="contact" className="py-20 bg-primary text-white">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-6">Contact Me</h2>
-        <p>If you’d like to get in touch, feel free to reach out via the following:</p>
+        <p>Let's get in touch:</p>
         <div className="mt-6 space-x-4">
           <a href="mailto:tanyabudhrani04@gmail.com" className="px-6 py-3 bg-gradient text-white rounded-lg border-gray-500 hover:bg-teal-500 hover:text-black transition-all border-2 shadow-xl hover:shadow-2xl border-transparent">
             Email
