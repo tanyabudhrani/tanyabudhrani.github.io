09845c727f452730730b5e04ffc73c725e8bb6a7
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-10 12:06:14 +0800

    Gradient

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,13 +7,13 @@ function Contact() {
         <h2 className="text-4xl font-bold mb-6">Contact Me</h2>
         <p>If you’d like to get in touch, feel free to reach out via the following:</p>
         <div className="mt-6 space-x-4">
-          <a href="mailto:tanyabudhrani04@gmail.com" className="bg-gradient hover:bg-secondary transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
+          <a href="mailto:tanyabudhrani04@gmail.com" className="bg-primary hover:bg-teal transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
             Email
           </a>
-          <a href="https://github.com/tanyabudhrani" target="_blank" rel="noopener noreferrer" className="bg-gradient hover:bg-secondary transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
+          <a href="https://github.com/tanyabudhrani" target="_blank" rel="noopener noreferrer" className="bg-primary hover:bg-teal transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
             GitHub
           </a>
-          <a href="https://www.linkedin.com/in/tanya-budhrani/" target="_blank" rel="noopener noreferrer" className="bg-gradient hover:bg-secondary transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
+          <a href="https://www.linkedin.com/in/tanya-budhrani/" target="_blank" rel="noopener noreferrer" className="bg-primary hover:bg-teal transition-colors duration-300 text-white font-bold py-2 px-4 rounded">
             LinkedIn
           </a>
         </div>
