29ec181a9652c7270bc8da41c496be2f37d8bb4f
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-10 14:08:44 +0800

    BG

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ const App = () => {
   if (isLoading) {
     return (
       <div className="bg-primary w-full overflow-hidden">
-        {/* Use your icon instead of the text */}
         <div className="flex items-center justify-center h-screen">
           <img src="/assets/lg512.png" alt="Loading Icon" className="w-50 h-50" />
         </div>
