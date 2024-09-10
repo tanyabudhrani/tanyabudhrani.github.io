2867768c47afb285eb024a96694805abce1f8421
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-10 11:39:36 +0800

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -44,7 +44,7 @@ const ExperienceCard = (props) => {
         <img
           src={props.logo}
           alt={props.organisation}
-          className="w-[52px] h-[52px] rounded-full z-[2]"
+          className="w-[45px] h-[45px] rounded-full z-[2]"
         />
         <h4 className="font-poppins font-semibold text-[20px] text-gradient leading-[32px] ml-2">
           {props.organisation}
