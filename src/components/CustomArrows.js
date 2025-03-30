cf44653c5cd017dd99dcdaebbba2c283c9f9eac4
tanyabudhrani <tanyabudhrani04@gmail.com>
2025-03-30 19:05:09 +0800

    Update CustomArrows.js

--- src/components/CustomArrows.js
+++ src/components/CustomArrows.js
@@ -1,30 +1,32 @@
-const CustomNextArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <motion.div
-        initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.6 }}
-        whileHover={{ scale: 1.2 }}
-        className={className}
-        style={{ ...style, display: "block", zIndex: 10 }}
-        onClick={onClick}
-      />
-    );
-  };
-  
-  const CustomPrevArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <motion.div
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.6 }}
-        whileHover={{ scale: 1.2 }}
-        className={className}
-        style={{ ...style, display: "block", zIndex: 10 }}
-        onClick={onClick}
-      />
-    );
-  };
-  
+import React from "react";
+import { motion } from "framer-motion";
+
+export const CustomNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6 }}
+      whileHover={{ scale: 1.2 }}
+      className={className}
+      style={{ ...style, display: "block", zIndex: 10 }}
+      onClick={onClick}
+    />
+  );
+};
+
+export const CustomPrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6 }}
+      whileHover={{ scale: 1.2 }}
+      className={className}
+      style={{ ...style, display: "block", zIndex: 10 }}
+      onClick={onClick}
+    />
+  );
+};
