65c8fa61a839cc5a444b9a95d11329e9c8ac6b36
tanyabudhrani <tanyabudhrani04@gmail.com>
2025-05-08 13:05:43 +0700

    blog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ function Navbar() {
           <a href="#experience" className="text-white hover:text-blue-400">Experience</a>
           <a href="#projects" className="text-white hover:text-blue-400">Projects</a>
           <a href="#extracurriculars" className="text-white hover:text-blue-400">Extracurriculars</a>
-          <a href="#writing" className="text-white hover:text-blue-400">Writing</a>
+          <a href="https://tanyasthoughts.netlify.app/" className="text-white hover:text-blue-400">Writing</a>
           <a href="#contact" className="text-white hover:text-blue-400">Contact</a>
         </div>
       </div>
